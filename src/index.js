7d1166600a2ebac163d69941b0c200b52a358e59
Heesu01 <rulru0413@naver.com>
2025-01-08 00:05:03 +0900

    setting: 테마 초기설정

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
+import { Theme } from "./styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/styles/Theme.js
@@ -0,0 +1,23 @@
+import { css } from "styled-components";
+
+export const Theme = {
+  fonts: {
+    logo: css`
+      /* font-family: ;
+      font-size: ;
+      font-weight: ; */
+    `,
+
+    default: css``,
+    title: css``,
+    other: css``,
+  },
+  colors: {
+    white: "#fff",
+    gray: "#9A9A9A",
+    gray2: "#E9E9E9",
+    black: "#000",
+    red: "#FF4040",
+  },
+  breakpoints: {},
+};
