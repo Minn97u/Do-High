7eeb91336ffb5f223fe34f907855063f332a2ffb
Heesu01 <rulru0413@naver.com>
2025-01-09 14:49:43 +0900

    fix: 오타 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Alarm from "./pages/ Alarm";
+import Alarm from "./pages/Alarm";
 import MyPage from "./pages/MyPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import BoardList from "./pages/BoardList";
