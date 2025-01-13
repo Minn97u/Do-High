39cec31d6ebbe7332cde5b3d4dfa5f8571dfb4a7
Heesu01 <rulru0413@naver.com>
2025-01-13 15:44:04 +0900

    hotfix: 배포 빌드 에러 해결

---
+++ src/pages/ExpList.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import backBtn from "../assets/backBtn.svg";
+
+const ExpList = () => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Header>
+        <BackButton onClick={() => navigate(-1)}>
+          <img src={backBtn} alt="뒤로가기" />
+        </BackButton>
+        <Title>경험치 내역</Title>
+      </Header>
+    </Container>
+  );
+};
+
+export default ExpList;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.colors.gray};
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 15px 0;
+  position: relative;
+`;
+
+const BackButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
+  position: absolute;
+  left: 20px;
+  top: 12px;
+`;
+
+const Title = styled.h1`
+  ${(props) => props.theme.fonts.semiBold};
+  font-size: 18px;
+  text-align: center;
+  flex: 1;
+`;
