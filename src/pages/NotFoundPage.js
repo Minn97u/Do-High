af433f781446b6787db6cd489ca9b4dfd889045d
Heesu01 <rulru0413@naver.com>
2025-01-15 16:35:50 +0900

    feat: 404 페이지 추가

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,76 @@
 import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 const NotFoundPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Logo src={logo} alt="logo" />
+      <ErrorCode>404</ErrorCode>
+      <Message>페이지를 찾을 수 없습니다.</Message>
+      <Description>
+        요청하신 페이지가 존재하지 않거나, <br /> 주소가 잘못되었습니다.
+      </Description>
+      <GoHomeButton onClick={handleGoHome}>홈으로 돌아가기</GoHomeButton>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.gray};
+  text-align: center;
+  padding: 20px;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ErrorCode = styled.h1`
+  font-size: 96px;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.mainC};
+`;
+
+const Message = styled.h2`
+  font-size: 24px;
+  margin: 10px 0;
+`;
+
+const Description = styled.p`
+  font-size: 16px;
+  margin-bottom: 20px;
+`;
+
+const GoHomeButton = styled.button`
+  padding: 12px 24px;
+  font-size: 16px;
+  background-color: ${(props) => props.theme.colors.mainC};
+  color: ${(props) => props.theme.colors.white};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: "#e55b2f";
+  }
+`;
