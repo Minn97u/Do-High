324cb973ad55221bbb44891db99328cacf37c5d9
Heesu01 <rulru0413@naver.com>
2025-01-16 11:34:40 +0900

    feat: Axios파일에 요청헤더 추가

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -4,3 +4,29 @@ export const Axios = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
+
+Axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/auth/login";
+    }
+    return Promise.reject(error);
+  }
+);
