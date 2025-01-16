5330bebd052d03e5981a400d068d22a898071edc
Heesu01 <rulru0413@naver.com>
2025-01-17 05:53:15 +0900

    hotfix: 배포 에러 수정

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "blaybus-4d648",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
