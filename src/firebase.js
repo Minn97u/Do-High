a30b99a8e0224367c6f25bf717ae7a2f21aa89f1
Heesu01 <rulru0413@naver.com>
2025-02-28 00:21:04 +0800

    hotfix: api키 env 담기

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCx1HO187922re9RHh8uvyagVh-FmgxcG0",
-  authDomain: "blaybus-4d648.firebaseapp.com",
-  projectId: "blaybus-4d648",
-  storageBucket: "blaybus-4d648.firebasestorage.app",
-  messagingSenderId: "321976292831",
-  appId: "1:321976292831:web:b84bf4159739fa366ce06a",
-  measurementId: "G-GFBYMNV6SV",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
