49c826de1f5a1f263a067b3c4ed8c7fec07ab1ad
Minn97u <kmg09190@naver.com>
2025-03-23 23:55:00 +0900

    fix: 수정

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -20,7 +20,7 @@ const SplashScreen = () => {
       as={motion.div}
       initial={{ opacity: 1 }} // 처음에는 완전 보이게
       animate={{ opacity: 0 }} // 점점 사라지는 효과
-      transition={{ duration: 1, ease: "easeIn" }} // 2초 동안 서서히 페이드아웃
+      transition={{ duration: 1, ease: "easeIn" }} // 1초 동안 서서히 페이드아웃
     >
       <Logo src={logo} alt="Company Logo" />
     </Container>
