ac09fbaa69d8f9a43233df36866fe6cfa458c437
Minn97u <kmg09190@naver.com>
2025-03-26 14:04:58 +0900

    refactor: api 코드 수정

--- src/api/BoardApi.js
+++ src/api/BoardApi.js
@@ -1,12 +1,18 @@
 import { Axios } from "./Axios";
 
-//게시글 전체 조회
-export const getPosts = async (sort = "LATEST") => {
+//게시글 전체 조회ㄴ
+// BoardApi.js
+export const getPosts = async (
+  page = 1,
+  size = 10,
+  sort = "createdAt,DESC"
+) => {
   try {
     const response = await Axios.get("/posts", {
-      params: { sort },
+      params: { page, size, sort },
     });
     if (response.data.responseType === "SUCCESS") {
+      // data: { totalPostCount, totalPages, currentPage, posts: [...] }
       return response.data.success;
     } else {
       throw new Error(response.data.error?.message);
@@ -19,7 +25,7 @@ export const getPosts = async (sort = "LATEST") => {
 //게시글 단건 조회
 export const getPostById = async (postId) => {
   try {
-    const response = await Axios.get(`/posts/${postId}`);
+    const response = await Axios.get("/posts/${postId}");
     if (response.data.responseType === "SUCCESS") {
       return response.data.success;
     } else {
@@ -48,7 +54,7 @@ export const createPost = async (title, content) => {
 //게시글 삭제
 export const deletePostById = async (postId) => {
   try {
-    const response = await Axios.delete(`/posts/${postId}`);
+    const response = await Axios.delete("/posts/${postId}");
     if (response.data.responseType === "SUCCESS") {
       return response.data.success;
     } else {
@@ -62,7 +68,7 @@ export const deletePostById = async (postId) => {
 // 게시글 수정
 export const updatePost = async (postId, title, content) => {
   try {
-    const response = await Axios.put(`/posts/${postId}`, { title, content });
+    const response = await Axios.put("/posts/${postId}", { title, content });
     if (response.data.responseType === "SUCCESS") {
       return response.data.success;
     } else {
