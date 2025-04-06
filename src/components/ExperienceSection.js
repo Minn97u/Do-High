8cf17a39126cdf672fc06e8c047d681b7980c8e9
Minn97u <kmg09190@naver.com>
2025-04-07 04:08:37 +0900

    fix: 필요없는 로직 삭제

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { getRecentExp } from "../api/ExpApi";
 import { getQuests } from "../api/QuestApi";
